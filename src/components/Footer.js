a9e0b8c7438f2db393fc5fd2b94d5ce92561577f
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-01-31 13:39:36 +0200

    fix: use correct color values

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,5 +3,5 @@ import React from 'react';
 export default function Footer() {
   return <footer className="section">
       <p>All trademarks are property of their respective owners in the US and other countries.</p>
-  </footer>;
+  </footer>
 }
