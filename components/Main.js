69d5f936f2d64056f9660684e1123f49254b0b4c
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-02-26 17:20:44 +0200

    add api route and link it to client side

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import styles from '../styles/components/_main.module.scss'
+import useSWR from 'swr'
 
 export default function Main() {
+  
+  const [games, setGames] = React.useState([])
+
+  const fetcher = url => fetch(url).then(r => r.json())
+  
+  
+  const { data, error } = useSWR('/api/hello', fetcher)
+  
+  // React.useEffect(() => {
+  //   data => setGames(data)
+  // },[])
+  
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  
+
+  console.log(games)
+  console.log(data)
+
   return <div>
     <article className={styles['main-layout']}>
       <div className={styles["games"]}>
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import axios from 'axios';
+
+export default async function handler(req, res) {
+
+  await axios({
+    url: "https://api.igdb.com/v4/games",
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Client-ID': '3gme7daiuncpui724oa7bo0abveygb',
+        'Authorization': 'Bearer 4xtwp8lwnvc6ez5k7y15yi0706l0br',
+    },
+    data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+
+  })
+  .then(response => {
+    const data = res.json(response.data)
+    res.status(200).end();
+    return data
+  })
+  .catch(err => {
+    res.json(err);
+    res.status(405).end();
+  })
 }
