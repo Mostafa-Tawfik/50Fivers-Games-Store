fd5b787534ef478456f7289c26bad52315544b1b
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-03-03 12:08:11 +0200

    add env key and remove unused apis

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    rawgkey: 'key=f4580a87832d4755b9778a0962d2af81',
+  },
 }
 
 module.exports = nextConfig
--- pages/api/homegames.js
+++ pages/api/homegames.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 export default async function handler(id, res) {
 
   const apiRoot= 'https://rawg.io/api/games'
-  const key = 'key=f4580a87832d4755b9778a0962d2af81'
 
-  await axios.get(`${apiRoot}?${key}&dates=2022-01-01,2022-04-01&ordering=-added`)
+  await axios.get(`${apiRoot}?${process.env.rawgkey}&dates=2022-01-01,2022-04-01&ordering=-added`)
   .then(response => {
     const data = res.json(response.data)
     res.status(200).end();
