afe9a2cfd1ba257a78af74d3e575d0c85f30f123
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-03-17 15:54:58 +0200

    remove no cover games from serach

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -7,18 +7,23 @@ function SearchResult(props) {
   const MainGames = props.matchedGame.map(g => {
     return (
       
-        <div  key={g.id} className={styles["games"]}>
-            
-            {g.cover ? <Link href={'/game/' + g.id}> 
+        <div key={g.id} className={styles["games"]}>
+
+            {g.cover && <Link href={'/game/' + g.id}> 
                 <img src={g.cover.url.replace('thumb', 'cover_big')} alt={g.name.substring(0,22)}></img>
-            </Link> : <Link href={'/game/' + g.id}><div className={styles['notfound']}>&#10060;</div></Link>}
-            <div className={styles["infos"]}>
+            </Link>}
+
+            {/* {g.cover ? <Link href={'/game/' + g.id}> 
+                <img src={g.cover.url.replace('thumb', 'cover_big')} alt={g.name.substring(0,22)}></img>
+            </Link> : <Link href={'/game/' + g.id}><div className={styles['notfound']}>&#10060;</div></Link>} */}
+            
+            {g.cover && <div className={styles["infos"]}>
                 {/* limit characters to 22 */}
                 <Link href={'/game/' + g.id}>
                     <p className={styles['gameName']}>{g.name.substring(0,22)}</p>
                 </Link>
                 {g.genres && <p className={styles["tag"]}>{g.genres[0].name}</p>}
-            </div>
+            </div>}
         </div>
       
     )
